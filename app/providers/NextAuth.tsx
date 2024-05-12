739b598b4f191236d859c432eaf3120723c0adcb
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2024-05-12 13:11:44 +0000

    NextAuthの導入方法に誤りがあったため修正

--- app/providers/NextAuth.tsx
+++ app/providers/NextAuth.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import React from 'react';
 import { ReactNode } from 'react';
 
-const NextAuthProvider = ({ children, session }: { children: ReactNode; session: Session }) => {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+const NextAuthProvider = ({ children }: { children: ReactNode }) => {
+  return <SessionProvider>{children}</SessionProvider>;
 };
 
 export default NextAuthProvider;
