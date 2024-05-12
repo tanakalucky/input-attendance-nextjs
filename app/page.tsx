bc1d727954a32ab68308e518adecc2c58b7ed86d
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2024-05-12 12:30:27 +0000

    トップページ作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,25 @@
-'use client';
-
-import { signIn, signOut, useSession } from 'next-auth/react';
+import Image from 'next/image';
 
 export default function Home() {
-  const session = useSession();
-
-  if (session.status === 'unauthenticated') {
-    return <button onClick={() => signIn('cognito')}>Sign in</button>;
-  }
-
-  if (session.status === 'loading') {
-    return <div>...loading</div>;
-  }
-
-  return <button onClick={() => signOut()}>Sign out</button>;
+  return (
+    <section className='w-full py-6'>
+      <div className='container mx-auto px-4 md:px-6'>
+        <div className='grid items-center gap-6 lg:grid-cols-2 lg:gap-12'>
+          <div className='space-y-4'>
+            <h1 className='text-3xl font-bold tracking-tight sm:text-4xl md:text-5xl lg:text-6xl'>
+              Save your time for chores
+            </h1>
+            <p className='text-gray-500 dark:text-gray-400 md:text-xl'>This is powerful tool for your work.</p>
+          </div>
+          <Image
+            alt='Hero'
+            className='mx-auto aspect-square overflow-hidden rounded-xl object-cover sm:w-full'
+            height='550'
+            src='/input_attendance.webp'
+            width='550'
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
